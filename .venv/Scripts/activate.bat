3683443d8572bfddc9d798d8d43e3f3f34120206
Eraj <anwaraeraj@gmail.com>
2024-09-17 22:42:51 -0500

    Serving Splash Page

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\eraja\Desktop\4353Fall24\.venv
+set VIRTUAL_ENV=C:\Users\18324\Documents\VSCodeProjects\4353Fall24\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
--- server.py
+++ server.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def test():
-    return "<h>test</h>"
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
 

ed30a34eda29c4281ca63204026188a1cd89bc83
Eraj <anwaraeraj@gmail.com>
2024-09-17 22:58:43 -0500

    Registration Page

--- server.py
+++ server.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def test():
+def splash_screen():
     return render_template('index.html')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        #handle incoming data
+        pass
+    else:
+        return render_template('registration.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
